816224418485abf2cbbfcbaa117e1513b536ccfa
Hitarth Mistry <hitarthmistry42@gmail.com>
2021-09-14 15:18:24 -0500

    P01A WIP
    Created Projectile code and visuals for Boss. Need to fully implement projectile and hit detection

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Projectile : MonoBehaviour
+{
+    // bullet
+    public GameObject bullet;
+
+    // bullet force
+    public float shootForce, upwardForce;
+
+    // Gun stats
+    public float timeBetweenShooting, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //reference
+    public Transform attackPoint;
+
+    // Graphics
+    public GameObject muzzleFlash;
+    public TextMeshProUGUI ammunitionDisplay;
+
+    //bug fixing
+    public bool allowInvoke = true;
+
+    private void Awake()
+    {
+        // make sure magazine is full
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+
+        // Set ammo display
+        if (ammunitionDisplay != null)
+        {
+            ammunitionDisplay.SetText(bulletsLeft / bulletsPerTap + " / " + magazineSize / bulletsPerTap);
+        }
+    }
+
+    private void MyInput()
+    {
+        // Check if allowed to hold fown button and take corresponding input
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Space);
+        else shooting = Input.GetKeyDown(KeyCode.Space);
+
+        // Reloading
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
+            Reload();
+        // Reload automatically when trying to shoot without ammo
+        if (readyToShoot && shooting && !reloading && bulletsLeft <= 0)
+            Reload();
+
+        // Shooting
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            // Set bullets shot to 0
+            bulletsShot = 0;
+
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //direction to fire
+        Vector3 direction = attackPoint.forward;
+
+        // Instantiate bullet/projectile
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        // rotate bullet to proper direction
+        currentBullet.transform.forward = direction.normalized;
+
+        // Add forces to bullet
+        currentBullet.GetComponent<Rigidbody>().AddForce(direction.normalized * shootForce, ForceMode.Impulse);
+
+        // Instantiate muzzle flash
+        if (muzzleFlash != null)
+        {
+            Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        }
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        //Invoke resetShot function
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+        // if mroe than on bulletsPerTap make sure to repeat shoot function
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+        {
+            Invoke("shoot", timeBetweenShots);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+
+}
